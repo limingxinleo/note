ae703b71bfb66a7be4b631a317224b352c993a4a
李铭昕 <715557344@qq.com>
2017-04-25 19:11:58 +0800

    * 修改安装nodejs的脚本

--- nodejs/install.sh
+++ nodejs/install.sh
@@ -3,7 +3,7 @@ version='7.9.0'
 wget https://npm.taobao.org/mirrors/node/v${version}/node-v${version}-linux-x64.tar.gz
 tar xzf node-v${version}-linux-x64.tar.gz
 mkdir -p /usr/local/nodejs/${version}
-mv -f node-v${version}-linux-x64/* /usr/local/nodejs/${version}
+cp -rf node-v${version}-linux-x64/* /usr/local/nodejs/${version}
 
 ln -sf /usr/local/nodejs/${version}/bin/node /usr/local/bin/node
 ln -sf /usr/local/nodejs/${version}/bin/npm /usr/local/bin/npm
@@ -12,3 +12,5 @@ echo checking nodejs:
 node -v
 echo checking npm:
 npm -v
+# 设置镜像源
+npm config set registry=http://registry.npm.taobao.org
