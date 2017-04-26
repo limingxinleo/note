901ed861707609f86d034f344d71ef0dff1e2ef8
李铭昕 <715557344@qq.com>
2017-04-26 18:34:34 +0800

    * 增加mongo安装脚本

---
+++ db/mongodb.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# https://www.mongodb.com/download-center#community
+package='mongodb-linux-x86_64-rhel70-3.4.4'
+version='3.4.4'
+dbpath='/usr/local/mongodb/db'
+logspath='/usr/local/mongodb/logs'
+startfile='/usr/local/mongodb/start.md'
+
+wget https://fastdl.mongodb.org/linux/${package}.tgz
+tar zxvf ${package}.tgz
+mkdir -p /usr/local/mongodb/${version}
+cp -rf ${package}/* /usr/local/mongodb/${version}
+mkdir -p ${dbpath}
+mkdir -p ${logspath}
+touch ${logspath}/mongodb.logs
+
+ln -sf /usr/local/mongodb/${version}/bin/mongod /usr/local/bin/mongod
+ln -sf /usr/local/mongodb/${version}/bin/mongo /usr/local/bin/mongo
+
+echo mongod --dbpath ${dbpath} --port 27017 --logpath ${logspath}/mongodb.logs --logappend & > ${startfile}
