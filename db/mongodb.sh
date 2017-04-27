d94e3e38c45cee2131d1ceac29a8534ae118b8b2
李铭昕 <715557344@qq.com>
2017-04-27 18:32:18 +0800

    * mongod

--- db/mongodb.sh
+++ db/mongodb.sh
@@ -5,6 +5,7 @@ version='3.4.4'
 dbpath='/usr/local/mongodb/db'
 logspath='/usr/local/mongodb/logs'
 startfile='/usr/local/mongodb/start.md'
+configfile='/usr/local/mongodb/default.conf'
 
 wget https://fastdl.mongodb.org/linux/${package}.tgz
 tar zxvf ${package}.tgz
@@ -18,3 +19,14 @@ ln -sf /usr/local/mongodb/${version}/bin/mongod /usr/local/bin/mongod
 ln -sf /usr/local/mongodb/${version}/bin/mongo /usr/local/bin/mongo
 
 echo mongod --dbpath ${dbpath} --port 27017 --logpath ${logspath}/mongodb.logs --logappend --fork > ${startfile}
+
+echo -e systemLog: \\n\
+  destination: file \\n\
+  path: ${logspath}/mongodb.logs \\n\
+  logAppend: true \\n\
+storage: \\n\
+  dbPath: ${dbpath} \\n\
+net:\\n\
+  bindIp: 127.0.0.1 \\n\
+processManagement: \\n\
+  fork: true > ${configfile}
