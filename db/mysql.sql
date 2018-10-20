8e54d9b6061e9a225c37518e8028e255a8733c03
李铭昕 <715557344@qq.com>
2018-10-20 17:56:16 +0800

    Fix

--- db/mysql.sql
+++ db/mysql.sql
@@ -8,8 +8,7 @@ ALTER TABLE `table` DROP PRIMARY KEY;
 ALTER TABLE `table` ADD PRIMARY KEY (`id`,`no`);
 
 ## 修改字段
-ALTER TABLE `table` MODIFY COLUMN `shop_id` bigint(20) unsigned
-NOT NULL DEFAULT '0' COMMENT '门店ID';
+ALTER TABLE `table` MODIFY COLUMN `shop_id` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '门店ID';
 
 ## 增加字段
 ALTER TABLE `table` ADD COLUMN `status` smallint(2) unsigned NOT NULL DEFAULT '1' COMMENT '状态';
